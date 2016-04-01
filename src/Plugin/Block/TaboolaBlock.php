c8ad812adefbef1ace16183749019db914488c3a
David Grayston <davidgrayston@googlemail.com>
2016-04-01 01:27:41 +0100

    Creating block derivatives

--- src/Plugin/Block/TaboolaBlock.php
+++ src/Plugin/Block/TaboolaBlock.php
@@ -8,7 +8,7 @@
 namespace Drupal\taboola\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 
 /**
  * Provides a Taboola block.
@@ -16,16 +16,19 @@ use Drupal\Core\Block\BlockBase;
  * @Block(
  *   id = "taboola_block",
  *   admin_label = @Translation("Taboola"),
+ *   deriver = "Drupal\taboola\Plugin\Derivative\TaboolaBlock"
  * )
  */
-class TaboolaBlock extends BlockBase {
+class TaboolaBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
    * {@inheritdoc}
    */
   public function build() {
+    // @todo get taboola block settings from exported entities.
+    $block_id = $this->getDerivativeId();
     return [
-      '#markup' => '',
+      '#markup' => 'Block ' . $block_id,
       '#cache' => [
         'contexts' => ['user.roles']
       ],
