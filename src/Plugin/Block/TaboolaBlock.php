74fc7970753bf3e95e3c5ba808e5f951e8caf5d8
David Grayston <davidgrayston@googlemail.com>
2016-04-01 18:14:58 +0100

    Block configuration form

--- src/Plugin/Block/TaboolaBlock.php
+++ src/Plugin/Block/TaboolaBlock.php
@@ -16,10 +16,52 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
  * @Block(
  *   id = "taboola_block",
  *   admin_label = @Translation("Taboola"),
- *   deriver = "Drupal\taboola\Plugin\Derivative\TaboolaBlock"
  * )
  */
-class TaboolaBlock extends BlockBase implements ContainerFactoryPluginInterface {
+class TaboolaBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+    $configuration = $this->getConfiguration();
+
+    $form['mode'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Mode'),
+      '#default_value' => isset($configuration['mode']) ? $configuration['mode'] : '',
+    );
+
+    $form['placement'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Placement'),
+      '#default_value' => isset($configuration['placement']) ? $configuration['placement'] : '',
+    );
+
+    $form['target_type'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Target Type'),
+      '#default_value' => isset($configuration['target_type']) ? $configuration['target_type'] : '',
+    );
+
+    $form['container'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Container'),
+      '#default_value' => isset($configuration['container']) ? $configuration['container'] : '',
+    );
+
+    return $form;
+  }
+
+  /**
+   * Overrides \Drupal\block\BlockBase::blockSubmit().
+   */
+  public function blockSubmit($form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    $this->setConfigurationValue('mode', $form_state->getValue('mode'));
+    $this->setConfigurationValue('placement', $form_state->getValue('placement'));
+    $this->setConfigurationValue('target_type', $form_state->getValue('target_type'));
+    $this->setConfigurationValue('container', $form_state->getValue('container'));
+  }
 
   /**
    * {@inheritdoc}
